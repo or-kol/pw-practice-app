5668816099b6eeee604127ff251abf79e76025d1
or-kol <orkol1988@gmail.com>
2025-10-05 13:18:30 +0300

    Clean up pie chart validation methods
    Removed unused methods related to pie chart validation and country button functionality.

--- tests/pageObjects/echarts/barChartModule.ts
+++ tests/pageObjects/echarts/barChartModule.ts
@@ -16,32 +16,6 @@ export class BarChartModule extends BasePage{
         await this.click(`a[title="Charts"]`);
         await this.click(`a:has-text("Echarts")`, 500);
     };
-    
-
-    async validatePieChartColors(expectedColors: {r: number, g: number, b: number}[]): Promise<void> {
-        const screenshotPath = await this.visualTesting.takeElementScreenshot(`${this.PIE_CHART_LOCATOR} canvas`, 'pie-chart-colors');
-        const extractedColors = await this.visualTesting.extractColorsFromImage(screenshotPath!);
-        const result = this.visualTesting.compareColorsToExpected(extractedColors, expectedColors, 50);
-        expect(result).toBeTruthy();
-    };
-    
-    async countryButtonFunctionality(x: number, y: number, screenshotName: string, countryName: string): Promise<void> {
-        await this.mouseInteraction.moveMouseInBoxedElement(`${this.PIE_CHART_LOCATOR} canvas`, x, y);
-        await this.mouseInteraction.mouseClick();
-        const screenshotPath = await this.visualTesting.takeElementScreenshot(`${this.PIE_CHART_LOCATOR} canvas`, screenshotName);
-        const extractedText = await this.visualTesting.extractTextFromImage(screenshotPath!);
-        const countryDisappeared = extractedText.toLowerCase().includes(countryName.toLowerCase());
-        expect(countryDisappeared).toBeFalsy();
-    };
-
-
-    async pieChartContentValidation(country: string, x: number, y: number, value: number, percentage: string): Promise<void> {
-        await this.mouseInteraction.moveMouseInBoxedElement(`${this.PIE_CHART_LOCATOR} canvas`, x, y);
-        const countryData = await this.getText(this.PIE_CHART_LOCATOR);
-        expect(countryData).toContain(country);
-        expect(countryData).toContain(value.toString());
-        expect(countryData).toContain(percentage);
-    };
 
 
     async validateBarChartColors(expectedColors: {r: number, g: number, b: number}[]): Promise<void> {
